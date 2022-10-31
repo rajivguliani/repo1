dbe2f47f6e910a9218d166882ae27515666d2d32
Rajiv Guliani <rajivguliani@yahoo.com>
2022-10-31 13:05:31 +0530

    Implemented Multi Product Promotion, all test cases are getting passed.

--- promotion_engine/src/com/promengine/config/PromotionConfig.java
+++ promotion_engine/src/com/promengine/config/PromotionConfig.java
@@ -2,10 +2,13 @@ package com.promengine.config;
 
 import static com.promengine.config.ProductConfig.PROD_A;
 import static com.promengine.config.ProductConfig.PROD_B;
+import static com.promengine.config.ProductConfig.PROD_C;
+import static com.promengine.config.ProductConfig.PROD_D;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import com.promengine.promotion.MultiProductPromotion;
 import com.promengine.promotion.Promotion;
 import com.promengine.promotion.SingleProductNItems;
 
@@ -20,6 +23,12 @@ public class PromotionConfig {
 
 		SingleProductNItems promotion2 = new SingleProductNItems(PROD_B, 2, 45);
 		promotionList.add(promotion2);
+
+		List<String> productList = new ArrayList<String>();
+		productList.add(PROD_C);
+		productList.add(PROD_D);
+		MultiProductPromotion multiProductPromotion1 = new MultiProductPromotion(productList, 30);
+		promotionList.add(multiProductPromotion1);
 
 		return promotionList;
 	}
--- promotion_engine/src/com/promengine/promotion/MultiProductPromotion.java
+++ promotion_engine/src/com/promengine/promotion/MultiProductPromotion.java
@@ -1,10 +1,70 @@
 package com.promengine.promotion;
 
+import java.util.List;
+import java.util.Map;
+
 import com.promengine.bean.Cart;
+import com.promengine.config.ProductConfig;
 
 public class MultiProductPromotion implements Promotion {
 
+	private List<String> products;
+	private double promotedPrice;
+
+	public MultiProductPromotion(List<String> pProducts, double pPromotedPrice) {
+		this.products = pProducts;
+		this.promotedPrice = pPromotedPrice;
+	}
+
+	/**
+	 * Promotion is only applicable if cart has at least one quantity of all the
+	 * items listed in promotion.
+	 */
 	@Override
 	public void applyPromotion(Cart pCart) {
+		double discount = 0;
+		if (pCart != null) {
+			int maxCount = getMaxApplicableCount(pCart);
+			if (maxCount > 0) {
+				double prodSum = 0;
+				for (String product : this.products) {
+					prodSum += ProductConfig.getPrice(product);
+				}
+				discount = maxCount * (prodSum - this.promotedPrice);
+				pCart.setCartTotal(pCart.getCartTotal() - discount);
+			}
+		}
+	}
+
+	/**
+	 * This method gives max applicable count on which promotion can be applied.
+	 * 
+	 * @param pCart
+	 * @return
+	 */
+	private int getMaxApplicableCount(Cart pCart) {
+		int maxCount = 0;
+		if (this.products != null) {
+			for (String product : this.products) {
+				if (pCart != null) {
+					Map<String, Integer> items = pCart.getItems();
+					if (items != null && items.containsKey(product)) {
+						int quantity = items.get(product);
+						if (maxCount == 0) {
+							maxCount = quantity;
+						} else {
+							if (maxCount > quantity) {
+								maxCount = quantity;
+							}
+						}
+					} else {
+						// Can't apply promotion as not all products are in cart
+						maxCount = -1;
+						break;
+					}
+				}
+			}
+		}
+		return maxCount;
 	}
 }
