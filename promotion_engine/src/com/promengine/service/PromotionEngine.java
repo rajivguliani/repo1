8b6029fa3aa3990a8c1168157c04b281e1120a1c
Rajiv Guliani <rajivguliani@yahoo.com>
2022-10-31 12:19:43 +0530

    Implemented Simple Cart without promotion - 3 test cases are getting passed.

--- promotion_engine/src/com/promengine/service/PromotionEngine.java
+++ promotion_engine/src/com/promengine/service/PromotionEngine.java
@@ -1,10 +1,34 @@
 package com.promengine.service;
 
+import java.util.Map;
+
 import com.promengine.bean.Cart;
+import com.promengine.config.ProductConfig;
 
 public class PromotionEngine {
 
 	public double calculatePrice(Cart pCart) {
+		// Calculate price without promotion
+		calculatePriceWithoutPromotion(pCart);
+
+		// TODO: Apply promotions
+
 		return pCart.getCartTotal();
+	}
+
+	private void calculatePriceWithoutPromotion(Cart pCart) {
+		double cartTotal = 0;
+		if (pCart != null) {
+			Map<String, Integer> items = pCart.getItems();
+			if (items != null) {
+				for (Map.Entry<String, Integer> entry : items.entrySet()) {
+					String product = entry.getKey();
+					int quantity = entry.getValue();
+					double prodPrice = ProductConfig.getPrice(product) * quantity;
+					cartTotal += prodPrice;
+				}
+			}
+		}
+		pCart.setCartTotal(cartTotal);
 	}
 }
